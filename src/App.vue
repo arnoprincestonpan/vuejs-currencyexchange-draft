69f9bc331a2cd04769a7c60627fa61ae9f55f2c4
Arno Princeston Pan <apan11@my.bcit.ca>
2023-04-17 23:35:26 -0700

    first commit

---
+++ src/App.vue
@@ -0,0 +1,28 @@
+<template>
+  <div>
+    <div class="title">Currency Exchange</div>
+    <div class="navigation">
+      <router-link to="/login">Login</router-link>
+      <router-link to="/contact">Contact</router-link>
+    </div>
+    <router-view></router-view>
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'App'
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
